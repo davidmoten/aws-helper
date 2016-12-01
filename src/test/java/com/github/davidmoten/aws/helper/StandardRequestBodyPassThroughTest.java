d30db76f4b7e8739b92b4245c0d43afbb385fd0f
Dave Moten <davidmoten@gmail.com>
2016-12-02 10:58:14 +1100

    dev

--- src/test/java/com/github/davidmoten/aws/helper/StandardRequestBodyPassThroughTest.java
+++ src/test/java/com/github/davidmoten/aws/helper/StandardRequestBodyPassThroughTest.java
@@ -1,6 +1,7 @@
 package com.github.davidmoten.aws.helper;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -15,21 +16,33 @@ public class StandardRequestBodyPassThroughTest {
         StandardRequestBodyPassThrough r = StandardRequestBodyPassThrough.from(null);
         assertFalse(r.accountId().isPresent());
     }
-    
+
     @Test
     public void testEmptyInput() {
         StandardRequestBodyPassThrough r = StandardRequestBodyPassThrough.from(Collections.emptyMap());
         assertFalse(r.accountId().isPresent());
         assertFalse(r.header("Some").isPresent());
         assertFalse(r.queryStringParameter("p").isPresent());
+        assertTrue(r.headers().isEmpty());
     }
-    
-    @Test
-    public void test() {
-        Map<String,Object> map = new HashMap<>();
-        
-    }
-    
 
-    
+    @Test
+    public void testEmptyParams() {
+        Map<String, Object> map = new HashMap<>();
+        Map<String, Object> params = new HashMap<>();
+        map.put("params", params);
+        StandardRequestBodyPassThrough r = StandardRequestBodyPassThrough.from(map);
+        assertFalse(r.queryStringParameter("a").isPresent());
+    }
+
+    @Test
+    public void testEmptyHeaders() {
+        Map<String, Object> map = new HashMap<>();
+        Map<String, Object> params = new HashMap<>();
+        map.put("params", params);
+        params.put("header", new HashMap<>());
+        StandardRequestBodyPassThrough r = StandardRequestBodyPassThrough.from(map);
+        assertFalse(r.header("a").isPresent());
+    }
+
 }
