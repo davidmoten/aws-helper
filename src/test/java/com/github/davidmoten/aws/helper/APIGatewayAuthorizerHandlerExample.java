1bfa2b7be9635bc254b4631959dcdbde0047fe16
Dave Moten <davidmoten@gmail.com>
2016-12-03 12:04:53 +1100

    add custom auth classes and fix dependency scope on junit

---
+++ src/test/java/com/github/davidmoten/aws/helper/APIGatewayAuthorizerHandlerExample.java
@@ -0,0 +1,76 @@
+/*
+* Copyright 2015-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+*
+*     http://aws.amazon.com/apache2.0/
+*
+* or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
+package com.github.davidmoten.aws.helper;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.github.davidmoten.aws.helper.auth.AuthPolicy;
+import com.github.davidmoten.aws.helper.auth.TokenAuthorizerContext;
+
+/**
+ * Handles IO for a Java Lambda function as a custom authorizer for API Gateway
+ *
+ * @author Jack Kohn
+ *
+ */
+public class APIGatewayAuthorizerHandlerExample implements RequestHandler<TokenAuthorizerContext, AuthPolicy> {
+
+    @Override
+    public AuthPolicy handleRequest(TokenAuthorizerContext input, Context context) {
+        LambdaLogger log = context.getLogger();
+
+    	String token = input.getAuthorizationToken();
+    	log.log("authorizationToken=" + token);
+
+    	// validate the incoming token
+        // and produce the principal user identifier associated with the token
+
+        // this could be accomplished in a number of ways:
+        // 1. Call out to OAuth provider
+        // 2. Decode a JWT token in-line
+        // 3. Lookup in a self-managed DB
+        String principalId = "xxxx";
+
+        // if the client token is not recognized or invalid
+        // you can send a 401 Unauthorized response to the client by failing like so:
+        // throw new RuntimeException("Unauthorized");
+
+        // if the token is valid, a policy should be generated which will allow or deny access to the client
+
+        // if access is denied, the client will receive a 403 Access Denied response
+        // if access is allowed, API Gateway will proceed with the back-end integration configured on the method that was called
+
+    	String methodArn = input.getMethodArn();
+    	log.log("methodArn=" + methodArn);
+    	String[] arnPartials = methodArn.split(":");
+    	String region = arnPartials[3];
+    	String awsAccountId = arnPartials[4];
+    	String[] apiGatewayArnPartials = arnPartials[5].split("/");
+    	String restApiId = apiGatewayArnPartials[0];
+    	String stage = apiGatewayArnPartials[1];
+    	String httpMethod = apiGatewayArnPartials[2];
+    	String resource = ""; // root resource
+    	if (apiGatewayArnPartials.length == 4) {
+    		resource = apiGatewayArnPartials[3];
+    	}
+
+        // this function must generate a policy that is associated with the recognized principal user identifier.
+        // depending on your use case, you might store policies in a DB, or generate them on the fly
+
+        // keep in mind, the policy is cached for 5 minutes by default (TTL is configurable in the authorizer)
+        // and will apply to subsequent calls to any method/resource in the RestApi
+        // made with the same token
+
+        // the example policy below denies access to all resources in the RestApi
+        return new AuthPolicy(principalId, AuthPolicy.PolicyDocument.getAllowAllPolicy(region, awsAccountId, restApiId, stage));
+    }
+
+}
